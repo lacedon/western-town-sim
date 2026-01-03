645353b77117dcbe19d97802cd785dcb125e7769
Viktor Kukharev <lacedon1@gmail.com>
2026-01-02 19:10:04 -0500

    Fix placing buildings

---
+++ src/common/is-odd.gd
@@ -0,0 +1,2 @@
+static func isOdd(value: int) -> bool:
+  return value % 2 == 1
--- src/components/builder/builder.gd
+++ src/components/builder/builder.gd
@@ -6,10 +6,12 @@ const testBuildingTexture22 = preload("res://resources/images/2x2.png")
 const testBuildingTexture23 = preload("res://resources/images/2x3.png")
 const testBuildingTexture33 = preload("res://resources/images/3x3.png")
 const BuildingScene = preload("res://src/components/building/building.tscn")
+const isOdd = preload("res://src/common/is-odd.gd")
 
 @export var buildingContainer: Node
 
 @onready var buildingNode: BuildingNode = $Building
+@onready var debugLabel: Label = $Label
 
 var _isBuildingStarted: bool = false
 
@@ -30,17 +32,20 @@ func _exit_tree() -> void:
   buildingNode.disconnect(buildingNode.areaEnteredExited.get_name(), _updateBuildingColoring)
 
 func _input(event: InputEvent) -> void:
-  if !_isBuildingStarted: return
-
   if event is InputEventMouseMotion:
-    buildingNode.position = (
-      Vector2(
-        round(event.position.x / GameConfig.tileSize.x),
-        round(event.position.y / GameConfig.tileSize.y)
-      ) + (Vector2(buildingNode.building.size) / 2)
-     ) * GameConfig.tileSize.x
+    if _isBuildingStarted:
+      buildingNode.position = Vector2(
+        (floor(event.position.x / GameConfig.tileSize.x) + (0.5 if isOdd.isOdd(buildingNode.building.size.x) else 0.)) * GameConfig.tileSize.x,
+        (floor(event.position.y / GameConfig.tileSize.y) + (0.5 if isOdd.isOdd(buildingNode.building.size.y) else 0.)) * GameConfig.tileSize.y,
+      )
+
+    debugLabel.text = str(event.position) + '\n' + str(Vector2i(
+      (floor(event.position.x / GameConfig.tileSize.x)),
+      (floor(event.position.y / GameConfig.tileSize.y)),
+    ))
+    debugLabel.position = event.position + Vector2(15, -25)
   elif event is InputEventMouseButton && event.is_pressed():
-    if buildingNode.canBePlaced():
+    if _isBuildingStarted && buildingNode.canBePlaced():
       placeBuilding()
       toggleBuilding()
 
