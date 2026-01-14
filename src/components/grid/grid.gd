13958102323030eb531d06a0fb3ed30ece7dfef2
Viktor Kukharev <lacedon1@gmail.com>
2026-01-13 20:17:50 -0500

    Rework grid

--- src/components/grid/grid.gd
+++ src/components/grid/grid.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 const lineSize: int = 2000
-const lineWidth: int = 1
+const lineWidth: int = 2
 const lineNumber: int = 100
+const color: Color = Color(0.75, 0.75, 0.75, 0.25)
 
 func _ready() -> void:
   for index in range(lineNumber):
@@ -12,7 +13,7 @@ func _ready() -> void:
 func _createLine(index: int, isHorizontal: bool) -> Line2D:
     var line = Line2D.new()
     line.width = lineWidth
-    line.default_color = Color.DIM_GRAY
+    line.default_color = color
     var coordinate = index * (GameConfig.tileSize.x if isHorizontal else GameConfig.tileSize.y)
     line.points = [Vector2(coordinate, 0), Vector2(coordinate, lineSize)] if isHorizontal else [Vector2(0, coordinate), Vector2(lineSize, coordinate)]
     return line
