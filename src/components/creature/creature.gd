a18e594e0f4bf2dd2bfc7bb03b9d7424619d5921
Viktor Kukharev <lacedon1@gmail.com>
2026-01-17 18:29:48 -0500

    Add base creature scripts

---
+++ src/components/creature/creature.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+class_name CreatureNode
+
+@export var creature: RCreature = null
---
+++ src/resource-definition/creature/creature.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name RCreature
+
+@export var name: String = ":: Creature ::"
+@export var texture: Texture2D
+@export var speed: float = 100.0
+@export var health: int = 100
