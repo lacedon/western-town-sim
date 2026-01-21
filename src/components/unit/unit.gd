24cf739e963e4bce623077ac0b81ff1937d27147
Viktor Kukharev <lacedon1@gmail.com>
2026-01-20 20:14:06 -0500

    Add unit wandering

--- src/components/unit/unit.gd
+++ src/components/unit/unit.gd
@@ -3,4 +3,47 @@ extends Node2D
 class_name UnitNode
 
 @export var unit: RUnit = null
-  
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var raycast: RayCast2D = $RayCast2D
+
+var _target_position: Vector2 = Vector2.ZERO
+var _is_moving: bool = false
+
+func _ready() -> void:
+  if !unit: return
+
+  sprite.texture = unit.texture
+  set_random_target_within_radius()
+
+func _process(delta: float) -> void:
+  if _is_moving: move_to_target(delta)
+  raycast.target_position = to_local(_target_position)
+
+func _generate_random_target_position() -> Vector2:
+  return position + Vector2(
+    randf_range(-unit.wanderingRadius, unit.wanderingRadius),
+    randf_range(-unit.wanderingRadius, unit.wanderingRadius)
+  )
+
+func set_random_target_within_radius() -> void:
+  var is_position_invalid: bool = true
+  while is_position_invalid:
+    var random_position: Vector2 = _generate_random_target_position()
+    raycast.target_position = to_local(random_position)
+    raycast.force_raycast_update()
+
+    is_position_invalid = raycast.is_colliding()
+    if !is_position_invalid: set_target_position(random_position)
+
+func set_target_position(target_position: Vector2) -> void:
+  _target_position = target_position
+  _is_moving = true
+
+func move_to_target(delta: float) -> void:
+  position = position.move_toward(_target_position, unit.speed * delta)
+  var difference: float = position.distance_to(_target_position)
+
+  if difference < 1.0:
+    position = _target_position
+    set_random_target_within_radius()
--- src/resource-definition/units/unit.gd
+++ src/resource-definition/units/unit.gd
@@ -4,5 +4,6 @@ class_name RUnit
 
 @export var name: String = ":: Unit ::"
 @export var texture: Texture2D
-@export var speed: float = 100.0
+@export var speed: float = 50.0
 @export var health: int = 100
+@export var wanderingRadius: float = 50.0
