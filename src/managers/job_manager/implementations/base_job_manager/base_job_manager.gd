1bd9a3b96595806620a520eb709b0c66d38c279b
Viktor Kukharev <lacedon1@gmail.com>
2026-03-13 21:52:18 -0400

    Add job manager implementation

--- src/managers/job_manager/implementations/base_job_manager/base_job_manager.gd
+++ src/managers/job_manager/implementations/base_job_manager/base_job_manager.gd
@@ -1,13 +1,32 @@
 extends "../../job_manager.gd"
 
-func add_jobs() -> void:
-  pass
+var jobs: Array[RJob] = []
 
-func remove_jobs() -> void:
-  pass
+func add_job(job: RJob) -> void:
+  jobs.append(job)
+  self.job_added.emit(job)
 
-func get_available_jobs() -> void:
-  pass
+func add_jobs(jobs_to_add: Array[RJob]) -> void:
+  for job in jobs_to_add:
+    add_job(job)
 
-func assign_job() -> void:
-  pass
+func remove_job(job: RJob) -> void:
+  jobs.erase(job)
+  self.job_deleted.emit(job)
+
+func remove_jobs(jobs_to_remove: Array[RJob]) -> void:
+  for job in jobs_to_remove:
+    remove_job(job)
+
+func assign_job(job: RJob, unit: RUnit) -> void:
+  job.assignee = unit
+  self.job_updated.emit(job)
+
+func is_job_available(job: RJob) -> bool:
+  return job.assignee == null
+
+func get_all_jobs() -> Array[RJob]:
+  return jobs
+
+func get_available_jobs() -> Array[RJob]:
+  return jobs.filter(is_job_available)
--- src/managers/job_manager/job_manager.gd
+++ src/managers/job_manager/job_manager.gd
@@ -7,7 +7,10 @@ signal job_updated(job: RJob)
 signal job_deleted(job: RJob)
 
 @abstract class AbstractJobManager:
-  @abstract func add_jobs() -> void
-  @abstract func remove_jobs() -> void
-  @abstract func get_available_jobs() -> void
-  @abstract func assign_job() -> void
+  @abstract func add_job(job: RJob) -> void
+  @abstract func add_jobs(jobs: Array[RJob]) -> void
+  @abstract func remove_job(job: RJob) -> void
+  @abstract func remove_jobs(jobs: Array[RJob]) -> void
+  @abstract func assign_job(job: RJob, unit: RUnit) -> void
+  @abstract func get_all_jobs() -> Array[RJob]
+  @abstract func get_available_jobs() -> Array[RJob]
